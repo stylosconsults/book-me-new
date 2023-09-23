636ce4682c621b3670655fd5c2032fcea8126ef0
Stylos Consults <it.stylosconsults58@gmail.com>
2023-09-23 09:44:42 +0200

    Update page.tsx

--- src/app/(user)/page.tsx
+++ src/app/(user)/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
       <HomeImageSlider />
       <CategoriesList />
       <PopularHotels />
-      <PopularCars />
+{/*       <PopularCars /> */}
       <HowItWorks />
     </main>
   );

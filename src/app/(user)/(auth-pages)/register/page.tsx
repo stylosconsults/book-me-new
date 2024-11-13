2a8b8ae3a7f0daae53e7e9bc2606a158f6ddc132
CK-EmbPro <kennydebrice2@gmail.com>
2024-11-13 19:00:22 +0200

    register-page

--- src/app/(user)/(auth-pages)/register/page.tsx
+++ src/app/(user)/(auth-pages)/register/page.tsx
@@ -1,39 +1,10 @@
 "use client";
-import UserForm from "@/components/organisms/UserForm";
-import { USER_TYPES } from "@/utils/user";
-import { useMutation } from "@tanstack/react-query";
-import { BASE_URL } from "@/lib/share";
-import { toast } from "react-toastify";
-import { ICreateUser } from "@/types/user.schema";
-import EmailSent from "@/components/molecules/EmailSent";
-import { createUser } from "@/utils/auth.api";
+
+import RegisterModal from "@/components/organisms/RegisterModal";
 
 export default function RegisterProperty() {
-  const { mutate, isLoading, isSuccess, data } = useMutation({
-    onSuccess() {
-      toast.success("Registration successful.");
-    },
-    onError(error: { message: string }) {
-      toast.error(error.message ?? "An error occurred during registration.");
-    },
-    mutationFn: createUser,
-  });
-
-  const handleSubmit = (data: ICreateUser) => {
-    mutate(data);
-  };
 
   return (
-    <div className="max-w-[400px] bg-white p-4 rounded-md mx-auto">
-      {isSuccess ? (
-        <EmailSent user={data?.user} token={data?.tokens.access.token} />
-      ) : (
-        <UserForm
-          isLoading={isLoading}
-          mutate={handleSubmit}
-          userType={USER_TYPES.HOTEL_ADMIN}
-        />
-      )}
-    </div>
+   <RegisterModal/>
   );
 }
